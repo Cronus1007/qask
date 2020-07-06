1bbaa1420f5c315e35902d2235eeb3702bfd76b1
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-06 14:56:33 +0530

    updated

--- models/subjects.js
+++ models/subjects.js
@@ -1,23 +1,27 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class subjects extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  subjects.init({
-    subjectName: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'subjects',
-  });
-  return subjects;
+    class subjects extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            subjects.hasMany(models.asking_qusetions, {
+                foreignKey: "SubjectId",
+                as: "asking_qusetions"
+            })
+        }
+    };
+    subjects.init({
+        subjectName: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'subjects',
+    });
+    return subjects;
 };
