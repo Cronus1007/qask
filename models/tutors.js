ac1770422226b7cdb045e9440d0c9d0ace1227ed
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-06 15:02:43 +0530

    updated

--- models/tutors.js
+++ models/tutors.js
@@ -1,28 +1,36 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class tutors extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  tutors.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    profilePic: DataTypes.STRING,
-    subjects: DataTypes.ARRAY(DataTypes.STRING),
-    grade: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'tutors',
-  });
-  return tutors;
+    class tutors extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            tutors.hasMany(models.tutor_ratings, {
+                foreignKey: "TutorId",
+                as: "tutor_ratings"
+            })
+            tutors.hasMany(models.answering_questions, {
+                foreignKey: "TutorId",
+                as: "answering_questions"
+            })
+        }
+    };
+    tutors.init({
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        profilePic: DataTypes.STRING,
+        subjects: DataTypes.ARRAY(DataTypes.STRING),
+        grade: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'tutors',
+    });
+    return tutors;
 };
