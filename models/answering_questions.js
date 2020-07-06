3ba246cfd37bbd6eac0ce45b80d238d613a61277
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-06 14:43:38 +0530

    updated

--- models/answering_questions.js
+++ models/answering_questions.js
@@ -1,31 +1,35 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class answering_questions extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  answering_questions.init({
-    stepOne: DataTypes.STRING,
-    stepTwo: DataTypes.STRING,
-    stepThree: DataTypes.STRING,
-    TutorId: DataTypes.INTEGER,
-    quesName: DataTypes.STRING,
-    explanation: DataTypes.STRING,
-    example: DataTypes.STRING,
-    files: DataTypes.ARRAY(DataTypes.STRING),
-    audio: DataTypes.ARRAY(DataTypes.STRING)
-  }, {
-    sequelize,
-    modelName: 'answering_questions',
-  });
-  return answering_questions;
+    class answering_questions extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            answering_questions.hasMany(models.asking_questions, {
+                foreignKey: "AnswerId",
+                as: "asking_questions"
+            })
+        }
+    };
+    answering_questions.init({
+        stepOne: DataTypes.STRING,
+        stepTwo: DataTypes.STRING,
+        stepThree: DataTypes.STRING,
+        TutorId: DataTypes.INTEGER,
+        quesName: DataTypes.STRING,
+        explanation: DataTypes.STRING,
+        example: DataTypes.STRING,
+        files: DataTypes.ARRAY(DataTypes.STRING),
+        audio: DataTypes.ARRAY(DataTypes.STRING)
+    }, {
+        sequelize,
+        modelName: 'answering_questions',
+    });
+    return answering_questions;
 };
