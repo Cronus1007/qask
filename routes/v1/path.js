316401ce1591f85e6d9058437660b643caddd19c
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-06 20:18:54 +0530

    updated

--- routes/v1/path.js
+++ routes/v1/path.js
@@ -1,6 +1,23 @@
 const router = require('express').Router()
 
-const students = require('./students/students')
+const students = require('./students/students');
+const questions = require('./asking_questions/asking_questions')
+const answers = require('./answering_questions/answering_questions');
+const subjects = require('./subjects/subjects');
+const doubts = require('./doubts/doubts');
+const classes = require('./classes/classes');
+const ratings = require('./tutor_ratings/tutor_ratings');
+const tutors = require('./tutors/tutors')
 
 router.use('/students', students)
+router.use('askingQuestions', questions);
+router.use('/answeringQuestions', answers);
+router.use('/subjects', subjects)
+router.use('/doubts', doubts);
+router.use('/classes', classes);
+router.use('/tutorRatings', ratings)
+router.use('/tutors', tutors)
+
+
+
 module.exports = router
