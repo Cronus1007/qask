73324c0979b87abc18def4066712f366670be9da
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 12:39:52 +0530

    updated

--- functions/classes/classes.js
+++ functions/classes/classes.js
@@ -30,5 +30,25 @@ module.exports = {
         } catch (error) {
             return res.sendStatus(500)
         }
+    },
+    UpdateClass: (req, res) => {
+        try {
+            Models.classes.update({
+                    class: req.body.class
+                }, { where: { id: req.params.id } })
+                .then(classes => { return res.send(classes) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+    },
+    DeleteClass: (req, res) => {
+        try {
+            Models.classes.destroy({
+                    where: { id: req.params.id }
+                })
+                .then(() => { return res.sendStatus(200) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
     }
 }
