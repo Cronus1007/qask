44e99143eff2f089e39767842d5a43eef9671cb5
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 13:04:50 +0530

    updated

--- routes/v1/classes/classes.js
+++ routes/v1/classes/classes.js
@@ -4,4 +4,9 @@ const classes = require('../../../functions/classes/classes')
 
 router.post('/create', (req, res) => classes.CreateClass(req, res));
 
+router.post('/create', (req, res) => classes.CreateClass(req, res));
+router.get('/read', (req, res) => classes.GetClass(req, res))
+router.put('/update', (req, res) => classes.UpdateClass(req, res))
+router.delete('/delete', (req, res) => classes.DeleteClass(req, res))
+
 module.exports = router
