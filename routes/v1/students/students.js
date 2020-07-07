ffbca87c0eb2d1707f060ed8b824f5558b8c9439
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 13:08:15 +0530

    updated

--- routes/v1/students/students.js
+++ routes/v1/students/students.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const students = require('../../../functions/students/students')
 
 router.post('/create', (req, res) => students.CreateStudent(req, res))
-
+router.get('/read', (req, res) => students.GetStudent(req, res))
+router.put('/update', (req, res) => students.UpdateStudent(req, res))
+router.delete('/delete', (req, res) => students.DeleteStudent(req, res))
 module.exports = router
