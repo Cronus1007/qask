206e65e0b38c6aebd19bde1a33fdeff1e4a55f37
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 13:01:45 +0530

    updated

--- routes/v1/answering_questions/answering_questions.js
+++ routes/v1/answering_questions/answering_questions.js
@@ -3,5 +3,8 @@ const router = require('express').Router();
 const answers = require('../../../functions/answering_questions/answering_questions');
 
 router.post('/create', (req, res) => answers.CreateAnswer(req, res));
+router.get('/read', (req, res) => answers.GetAnswer(req, res))
+router.put('/update', (req, res) => answers.UpdateAnswer(req, res))
+router.delete('/delete', (req, res) => answers.DeleteAnswer(req, res))
 
 module.exports = router
