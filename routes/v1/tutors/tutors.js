ace5b2352ddd0337be71948db11e8125348d225e
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 13:14:00 +0530

    updated

--- routes/v1/tutors/tutors.js
+++ routes/v1/tutors/tutors.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 const tutors = require('../../../functions/tutors/tutors')
 
 router.post('/create', (req, res) => tutors.CreateTutor(req, res));
-
+router.delete('/delete', (req, res) => tutors.DeleteTutor(req, res))
+router.get('/read', (req, res) => tutors.GetTutor(req, res))
+router.put('/update', (req, res) => tutors.UpdateTutor(req, res))
 module.exports = router
