3f7f22a6203a75f5ee26b78739ccc78f7b447f07
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 13:09:59 +0530

    updated

--- routes/v1/subjects/subjects.js
+++ routes/v1/subjects/subjects.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 const subjects = require('../../../functions/subjects/subjects')
 
 router.post('/create', (req, res) => subjects.CreateSubject(req, res));
-
+router.delete('/delete', (req, res) => subjects.DeleteSubject(req, res))
+router.get('/read', (req, res) => subjects.GetSubject(req, res))
+router.put('/update', (req, res) => subjects.UpdateSubject(req, res))
 module.exports = router
