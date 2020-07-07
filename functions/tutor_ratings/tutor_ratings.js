e3c078490079e25cbef358399ffdfab715f18359
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 12:55:04 +0530

    updated

--- functions/tutor_ratings/tutor_ratings.js
+++ functions/tutor_ratings/tutor_ratings.js
@@ -20,5 +20,39 @@ module.exports = {
                 message: "failed"
             })
         }
+    },
+    GetRating: (req, res) => {
+        try {
+            Models.tutor_ratings.findOne({
+                    where: { id: req.params.id }
+                })
+                .then(rating => { return res.send(rating) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+    },
+    UpdateRating: (req, res) => {
+        try {
+            Models.tutor_ratings.update({
+                TutorId: req.body.TutorId,
+                StudentId: req.body.StudentId,
+                Comments: req.body.Comments,
+                rating: req.body.rating
+            }, {
+                where: { id: req.params.id }
+            })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+    },
+    DeleteRating: (req, res) => {
+        try {
+            Models.tutor_ratings.destroy({
+                    where: { id: req.params.id }
+                })
+                .then(() => { return res.sendStatus(200) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
     }
 }
