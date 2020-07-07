ae0f7f15f2c1f72cf79e57670a57e7dd73ce0df0
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 13:12:48 +0530

    updated

--- routes/v1/tutor_ratings/tutor_ratings.js
+++ routes/v1/tutor_ratings/tutor_ratings.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 const ratings = require('../../../functions/tutor_ratings/tutor_ratings')
 
 router.post('/create', (req, res) => ratings.CreateRating(req, res));
-
+router.delete('/delete', (req, res) => ratings.DeleteRating(req, res))
+router.get('/read', (req, res) => ratings.GetRating(req, res))
+router.put('/update', (req, res) => ratings.UpdateRating(req, res))
 module.exports = router
