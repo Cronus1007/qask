b8cb9ee89c0799d8ba53d9d8ebac0585f74ef259
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 12:47:24 +0530

    updated

--- functions/students/students.js
+++ functions/students/students.js
@@ -19,5 +19,41 @@ module.exports = {
         } catch (error) {
             res.send({ message: "failed" })
         }
+    },
+    GetStudent: (req, res) => {
+        try {
+            Models.Students.findOne({
+                    where: { id: req.params.id }
+                })
+                .then(student => { return res.send(student) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+    },
+    UpdateStudent: (req, res) => {
+        try {
+            Models.Students.update({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                profilePic: req.body.profilePic,
+                subjects: req.body.subjects,
+                grade: req.body.grade
+            }, {
+                where: { id: req.params.id }
+            })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+    },
+    DeleteStudent: (req, res) => {
+        try {
+            Models.Students.destroy({
+                    where: { id: req.params.id }
+                })
+                .then(() => { return res.sendStatus(200) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
     }
 }
