9819632f8f7b4487fbf50fdec4556c8d21e042f1
Cronus1007 <lit2019058@iiitl.ac.in>
2020-07-07 12:58:55 +0530

    updated

--- functions/tutors/tutors.js
+++ functions/tutors/tutors.js
@@ -23,5 +23,42 @@ module.exports = {
                 message: "failed"
             })
         }
+    },
+    GetTutor: (req, res) => {
+        try {
+            Models.tutors.findOne({
+                    where: { id: req.params.id }
+                })
+                .then(tutor => { return res.send(tutor) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+    },
+    UpdateTutor: (req, res) => {
+        try {
+            Models.tutors.update({
+                    name: req.body.name,
+                    email: req.body.email,
+                    profilePic: req.body.profilePic,
+                    subjects: req.body.subjects,
+                    grade: req.body.grade,
+                    password: req.body.password
+                }, {
+                    where: { id: req.params.id }
+                })
+                .then(tutor => { return res.send(tutor) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+    },
+    DeleteTutor: (req, res) => {
+        try {
+            Models.tutors.destroy({
+                    where: { id: req.params.id }
+                })
+                .then(() => { return res.sendStatus(200) })
+        } catch (error) {
+            return res.sendStatus(500)
+        }
     }
 }
